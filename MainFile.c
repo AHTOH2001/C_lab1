1678d3665475f4d598f08790f7ae3c025226a861
AHTOH2001 <antonlapytko@gmail.com>
2020-02-19 20:10:01 +0300

    19.02

--- MainFile.c
+++ MainFile.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 #include <conio.h>
 #include "headers.h"
-int main()
-{
-    printf ("Choose the problem(0 to exit):\n");
-    printf ("1. problem 1 version 1\n");
-    printf ("2. problem 1 version 2\n");
+
+int main() {
+    printf("Choose the problem(0 to exit):\n");
+    printf("1. problem 1 version 1\n");
+    printf("2. problem 1 version 2\n");
     char c = '*';
-    while (c<'0' || c>'2')
-    {
+    while (c < '0' || c > '2') {
         c = getch();
-        if (c=='1') version1 (); 
-        else
-        if (c=='2') version2 ();
-        else
-        if (c=='0') 
-        {
-        printf ("Successfully exit\n");
-        break;
-        }
-        else
-        printf ("Error\n");
+        if (c == '1') version1();
+        else if (c == '2') version2();
+        else if (c == '0') {
+            printf("Successfully exit\n");
+            break;
+        } else
+            printf("Error\n");
     }
 
     return 0;
--- lab1z1v18.1.c
+++ lab1z1v18.1.c
@@ -1,75 +1,70 @@
 #include <stdio.h>
 #include <math.h>
 #include <limits.h>
-int len1 (int x)
-{
+
+int len1(int x) {
     int d = 0;
-    if (x==0) return 1;
-    while (x>0)
-    {
-        x/=10;
+    if (x == 0) return 1;
+    while (x > 0) {
+        x /= 10;
         d++;
     }
     return d;
 }
-void spaces1(int x)
-{
+
+void spaces1(int x) {
     int i = 0;
-    for (i = 0;i<x;i++)
+    for (i = 0; i < x; i++)
         printf(" ");
 }
-void tire1(int x)
-{
+
+void tire1(int x) {
     int i = 0;
-    for (i = 0;i<x;i++)
+    for (i = 0; i < x; i++)
         printf("-");
 }
-int max1 (int x, int y)
-{
-    if (x>y) return x;
+
+int max1(int x, int y) {
+    if (x > y) return x;
     else return y;
 }
-int version1()
-{
-    int x,y;
-    printf ("Vvedite dva natural'nih chisla\n");
-    scanf ("%d %d",&x,&y);
-    if (x<=0 || y<=0) 
-    {
-        printf ("\nNe vnaturi");
+
+int version1() {
+    int x, y;
+    printf("Vvedite dva natural'nih chisla\n");
+    scanf("%d %d", &x, &y);
+    if (x <= 0 || y <= 0) {
+        printf("\nNe vnaturi");
         return 0;
     }
-    if (x>INT_MAX/y || y>INT_MAX/x)
-    {
-        printf ("\nPerestaralsya malex");
+    if (x > INT_MAX / y || y > INT_MAX / x) {
+        printf("\nPerestaralsya malex");
         return 0;
     }
-    int dx,dy,ans = x*y;
+    int dx, dy, ans = x * y;
     dx = len1(x);
-    spaces1(20-dx);
-    printf ("%d\n",x);
+    spaces1(20 - dx);
+    printf("%d\n", x);
     dy = len1(y);
-    spaces1(20-max1(dx,dy)-1);
-    printf ("x\n");
-    spaces1(20-dy);
-    printf ("%d\n",y);
-    int da = len1 (ans);
-    spaces1(20-da-dy+1);
-    tire1 (da+dy-1);
-    printf ("\n");
+    spaces1(20 - max1(dx, dy) - 1);
+    printf("x\n");
+    spaces1(20 - dy);
+    printf("%d\n", y);
+    int da = len1(ans);
+    spaces1(20 - da - dy + 1);
+    tire1(da + dy - 1);
+    printf("\n");
     int i = 0;
-    while (1)
-    {
-        int v = y % 10, d = len1(v*x);
-        spaces1 (20-d-i);
-        printf ("%d",v*x);
-        spaces1 (i);
-        printf ("\n");
-        if (y/10==0)
-        {
-            spaces1 (20-da-i);
-            tire1 (da+i);
-            printf ("\n");
+    while (1) {
+        int v = y % 10, d = len1(v * x);
+        spaces1(20 - d - i);
+        printf("%d", v * x);
+        spaces1(i);
+        printf("\n");
+        if (y / 10 == 0) {
+            spaces1(20 - da - i);
+            tire1(da + i);
+            printf("\n");
             break;
         }
         /*
@@ -82,9 +77,9 @@ int version1()
         y /= 10;
         i++;
     }
-    spaces1 (20-da);
-    printf ("%d\n",ans);
+    spaces1(20 - da);
+    printf("%d\n", ans);
 
-    
+
     return 0;
 }
